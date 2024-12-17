e1d4b946fd71673f7448155601add277ef7de303
SachinKiragi <sachinkiragi8@gmail.com>
2024-12-18 02:10:21 +0530

    added React.memo on video to avoid unnecessory re-rendering of videos when room comp changes which brokes the  video streaming of other user

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -3,21 +3,20 @@ import io from "socket.io-client";
 import Peer from "simple-peer";
 import styled from "styled-components";
 
-
 const Wrapper = styled.div`
   display: flex;
-  height: 100vh; 
+  height: 100vh;
   width: 100%;
   justify-content: space-evenly;
   padding: 1rem;
 `;
 
 const Container = styled.div`
-  flex: 0 0 65%; 
+  flex: 0 0 65%;
   border: 1px solid red;
   display: flex;
-  flex-wrap:wrap;
-  gap:1rem;
+  flex-wrap: wrap;
+  gap: 1rem;
   padding: 1rem;
   height: fit-content;
   justify-content: space-evenly;
@@ -39,240 +38,185 @@ const MessageBox = styled.div`
   -ms-overflow-style: none;
 `;
 
-
 const StyledVideo = styled.video`
-    height:20rem;
-    width:26rem;
-    border: 2px solid black;
-    object-fit: cover;
-     
+  height: 20rem;
+  width: 26rem;
+  border: 2px solid black;
+  object-fit: cover;
 `;
 
-const Video = (props) => {
-    const ref = useRef();
+const Video = React.memo((props) => {
+  const ref = useRef();
 
-    useEffect(() => {
-        props.peer.on("stream", stream => {
-            ref.current.srcObject = stream;
-        })
-    }, []);
+  useEffect(() => {
+    props.peer.on("stream", (stream) => {
+      if (ref.current) {
+        ref.current.srcObject = stream;
+      }
+    });
+  }, [props.peer]);
 
-    return (
-        <StyledVideo playsInline autoPlay ref={ref} />
-    );
-}
-
+  return <StyledVideo playsInline autoPlay ref={ref} />;
+});
 
 const videoConstraints = {
-    height: window.innerHeight / 2,
-    width: window.innerWidth / 2
+  height: window.innerHeight / 2,
+  width: window.innerWidth / 2,
 };
 
 const Room = (props) => {
-    const [peers, setPeers] = useState([]);
-    const socketRef = useRef();
-    const userVideo = useRef();
-    const peersRef = useRef([]);
-    const [messages, setmessages] = useState([]);
-    const [inputMessage, setInputMessage] = useState('');
-    const roomID = props.match.params.roomID;
+  const [peers, setPeers] = useState([]);
+  const socketRef = useRef();
+  const userVideo = useRef();
+  const peersRef = useRef([]);
+  const [messages, setMessages] = useState([]);
+  const [inputMessage, setInputMessage] = useState("");
+  const roomID = props.match.params.roomID;
 
-    useEffect(async() => {
-        socketRef.current = io.connect("https://192.168.29.188:8181");
-        await navigator.mediaDevices.getUserMedia({ video: videoConstraints, audio: true }).then(stream => {
-            userVideo.current.srcObject = stream;
-            socketRef.current.emit("join room", roomID);
-            socketRef.current.on("all users", users => {
-                const peers = [];
-                users.forEach(userID => {
-                    const peer = createPeer(userID, socketRef.current.id, stream);
-                    peersRef.current.push({
-                        peerID: userID,
-                        peer,
-                    })
-                    peers.push(peer);
-                })
-                setPeers(peers);
-                console.log("PEERRSS: ", peers);
-                
-            })
-
-            socketRef.current.on("user joined", payload => {
-                const peer = addPeer(payload.signal, payload.callerID, stream);
-                peersRef.current.push({
-                    peerID: payload.callerID,
-                    peer,
-                })
-
-                setPeers(users => [...users, peer]);
-                console.log("user", peers);
-                
+  useEffect(() => {
+    socketRef.current = io.connect("https://192.168.29.188:8181");
+    navigator.mediaDevices
+      .getUserMedia({ video: videoConstraints, audio: false })
+      .then((stream) => {
+        userVideo.current.srcObject = stream;
+        socketRef.current.emit("join room", roomID);
+        socketRef.current.on("all users", (users) => {
+          const peers = [];
+          users.forEach((userID) => {
+            const peer = createPeer(userID, socketRef.current.id, stream);
+            peersRef.current.push({
+              peerID: userID,
+              peer,
             });
-
-            socketRef.current.on("receiving returned signal", payload => {
-                const item = peersRef.current.find(p => p.peerID === payload.id);
-                item.peer.signal(payload.signal);
-            });
-
-        })
-    }, []);
-
-    // const iceServers = [
-    //     {
-    //         urls: 'stun:stun.l.google.com:19302', // Google's public STUN server
-    //     },
-    //     // Add TURN server configuration if needed
-    //     // {
-    //     //     urls: 'turn:your-turn-server-url',
-    //     //     username: 'your-username',
-    //     //     credential: 'your-credential'
-    //     // },
-    // ];
-    
-
-        const iceServers = [
-            {
-                urls:[
-                  'stun:stun.l.google.com:19302',
-                  'stun:stun1.l.google.com:19302'
-                ]
-            }
-        ]
-
-    function createPeer(userToSignal, callerID, stream) {
-        const peer = new Peer({
-            initiator: true,
-            trickle: false,
-            config: { iceServers }, // Add ICE servers here,
-            stream,
+            peers.push(peer);
+          });
+          setPeers(peers);
         });
 
-        peer.on("signal", signal => {
-            socketRef.current.emit("sending signal", { userToSignal, callerID, signal })
-        })
-
-        console.log("peer", peer);
-        
-
-        return peer;
-    }
-
-    function addPeer(incomingSignal, callerID, stream) {
-        const peer = new Peer({
-            initiator: false,
-            trickle: false,
-            config: { iceServers }, // Include ICE servers
-            stream,
+        socketRef.current.on("user joined", (payload) => {
+          const peer = addPeer(payload.signal, payload.callerID, stream);
+          peersRef.current.push({
+            peerID: payload.callerID,
+            peer,
+          });
+          setPeers((users) => [...users, peer]);
         });
-    
-        peer.on("signal", signal => {
-            socketRef.current.emit("returning signal", { signal, callerID });
+
+        socketRef.current.on("receiving returned signal", (payload) => {
+          const item = peersRef.current.find((p) => p.peerID === payload.id);
+          item.peer.signal(payload.signal);
         });
-    
-        peer.on("stream", stream => {
-            console.log("Stream received from peer:", stream);
-        });
-    
-        peer.signal(incomingSignal);
-    
-        return peer;
-    }
-    
-    // Debug in Video Component
-    const Video = (props) => {
-        const ref = useRef();
-    
-        useEffect(() => {
-            props.peer.on("stream", stream => {
-                console.log("Video component stream received:", stream);
-                ref.current.srcObject = stream;
-            });
-        }, [props.peer]);
-    
-        return <StyledVideo playsInline autoPlay ref={ref} />;
-    };
-    
+      });
+  }, []);
 
+  const iceServers = [
+    {
+      urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"],
+    },
+  ];
 
-    function sendMessage(){
-        console.log("message sent\n");
-        
-        socketRef.current.emit("send message", {roomID, message: inputMessage});
-        setInputMessage('')
-    }
+  function createPeer(userToSignal, callerID, stream) {
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      config: { iceServers },
+      stream,
+    });
 
-    useEffect(()=>{
-        socketRef.current.on("receive message", data => {
-            console.log(`${data.from} says: ${data.message}`);
+    peer.on("signal", (signal) => {
+      socketRef.current.emit("sending signal", { userToSignal, callerID, signal });
+    });
 
-            setmessages(prevMessages => [...prevMessages, data]);
-            console.log(messages);
-            
-        });
-    }, [])
+    return peer;
+  }
 
+  function addPeer(incomingSignal, callerID, stream) {
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      config: { iceServers },
+      stream,
+    });
 
-    function createMessageDiv(msg){
-        return (
-            <div style={{margin:"1rem"}}>
-                <h5>:{msg.from}</h5>
-                <h3 style={{margin:".5rem"}}>{msg.message}</h3>
-            </div>
-        )
-    }
+    peer.on("signal", (signal) => {
+      socketRef.current.emit("returning signal", { signal, callerID });
+    });
 
+    peer.signal(incomingSignal);
 
+    return peer;
+  }
+
+  function sendMessage() {
+    socketRef.current.emit("send message", { roomID, message: inputMessage });
+    setInputMessage("");
+  }
+
+  useEffect(() => {
+    socketRef.current.on("receive message", (data) => {
+      setMessages((prevMessages) => [...prevMessages, data]);
+    });
+  }, []);
+
+  function createMessageDiv(msg) {
     return (
-        <Wrapper style={{display:"flex", gap:"2rem"}}>
-
-            <Container>
-                <StyledVideo muted ref={userVideo} autoPlay playsInline />
-                {peers.map((peer, index) => {
-                    return (
-                        <Video key={index} peer={peer} />
-                    );
-                })}
-            </Container>
-            
-            <MessageBox>
-            <div style={{ overflowY: "scroll", height: "calc(100% - 3rem)", scrollbarWidth: "none",
-    msOverflowStyle: "none" }}>
-                {messages.map(msg => {
-                    return createMessageDiv(msg);
-                })}
-            </div>
-
-            <div
-                style={{
-                    border: "2px solid red",
-                    height: "3rem",
-                    width: "100%",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "space-between",
-                    position: "absolute",
-                    bottom: "0",
-                    backgroundColor: "white",
-                }}
-            >
-                <input
-                    value={inputMessage}
-                    onChange={(e) => setInputMessage(e.target.value)}
-                    style={{ width: "75%", height: "100%" }}
-                    placeholder="Enter your message"
-                />
-                <button
-                    style={{ width: "20%", height: "100%" }}
-                    onClick={()=>inputMessage.length ? sendMessage(): ''}
-                >
-                    Send
-                </button>
-            </div>
-        </MessageBox>
-
-        </Wrapper>
-
+      <div style={{ margin: "1rem" }} key={msg.from + msg.message}>
+        <h5>:{msg.from}</h5>
+        <h3 style={{ margin: ".5rem" }}>{msg.message}</h3>
+      </div>
     );
+  }
+
+  return (
+    <Wrapper style={{ display: "flex", gap: "2rem" }}>
+      <Container>
+        <StyledVideo muted ref={userVideo} autoPlay playsInline />
+        {peers.map((peer, index) => {
+          return <Video key={index} peer={peer} />;
+        })}
+      </Container>
+
+      <MessageBox>
+        <div
+          style={{
+            overflowY: "scroll",
+            height: "calc(100% - 3rem)",
+            scrollbarWidth: "none",
+            msOverflowStyle: "none",
+          }}
+        >
+          {messages.map((msg) => createMessageDiv(msg))}
+        </div>
+
+        <div
+          style={{
+            border: "2px solid red",
+            height: "3rem",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            position: "absolute",
+            bottom: "0",
+            backgroundColor: "white",
+          }}
+        >
+          <input
+            value={inputMessage}
+            onChange={(e) => setInputMessage(e.target.value)}
+            style={{ width: "75%", height: "100%" }}
+            placeholder="Enter your message"
+          />
+          <button
+            style={{ width: "20%", height: "100%" }}
+            onClick={() => (inputMessage.length ? sendMessage() : "")}
+          >
+            Send
+          </button>
+        </div>
+      </MessageBox>
+    </Wrapper>
+  );
 };
 
 export default Room;
